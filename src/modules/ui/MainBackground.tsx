e30f4e530c70593218424ccdcf3ca56cb40112d0
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-02-03 19:17:23 -0300

    reduced the quality of background image to allow faster download when entering the website

--- src/modules/ui/MainBackground.tsx
+++ src/modules/ui/MainBackground.tsx
@@ -2,7 +2,7 @@
 
 export default function MainBackground() {
     return(
-        <div className=" w-screen h-screen bg-[url(/mainBg.png)] bg-cover 
+        <div className=" w-screen h-screen bg-[url(/mainBg.jpg)] bg-cover 
             bg-center top-0 left-0 -z-10 fixed">
         </div>
     )
